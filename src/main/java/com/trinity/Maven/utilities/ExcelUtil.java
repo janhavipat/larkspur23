7c2dafda2ea4910b8109bc0348cc80a1b062dc85
janhavipat <pjanhavi110@gmail.com>
2024-05-21 15:53:35 +0530

    Adding ReadExcel Method

--- src/main/java/com/trinity/Maven/utilities/ExcelUtil.java
+++ src/main/java/com/trinity/Maven/utilities/ExcelUtil.java
@@ -1,5 +1,33 @@
 package com.trinity.Maven.utilities;
 
-public class ExcelUtil {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtil{
+	
+	public void readExcel(String filePath)  {
+		FileInputStream  fis = null ;
+try {
+   new FileInputStream(filePath);
+} catch (FileNotFoundException e) {
+	e.printStackTrace();
+}
+
+Workbook book =null;
+
+  try {
+	 book = new XSSFWorkbook(fis);
+} catch (IOException e) {
+	e.printStackTrace();
+}
+  
+ Sheet sheet = book.getSheet("");
+
+	}
 
 }
